5a0fa288904375d071416dcec5eb40f228a88f66
Anna Ivanchenko <bubachka1990@gmail.com>
2026-07-20 02:56:47 +0300

    feat: add auth routes, register and login

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -7,7 +7,7 @@ export const getAllNotes = async (req, res) => {
   const pageNum = Number(page);
   const perPageNum = Number(perPage);
 
-  const query = Note.find();
+  const query = Note.find({ userId: req.user._id });
 
   if (tag) {
     query.where({ tag });
@@ -40,7 +40,10 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
+  });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -48,16 +51,26 @@ export const getNoteById = async (req, res) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
   res.status(201).json(note);
 };
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate(noteId, req.body, {
-    returnDocument: 'after',
-    runValidators: true,
-  });
+  const note = await Note.findByIdAndUpdate(
+    {
+      _id: noteId,
+      userId: req.user._id,
+    },
+    req.body,
+    {
+      returnDocument: 'after',
+      runValidators: true,
+    },
+  );
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -66,7 +79,10 @@ export const updateNote = async (req, res) => {
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndDelete(noteId);
+  const note = await Note.findByIdAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
